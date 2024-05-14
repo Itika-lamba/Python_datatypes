6d7dd392feb2d7df4030cc7952693fe15c22fca2
Itika-Lamba <itika@trigvent.com>
2024-05-14 15:06:09 +0530

    worked with list

---
+++ List.py
@@ -0,0 +1,13 @@
+str1="hello"
+print(str1)
+# list in phython 
+list=["hi","world",['multi d','list']]
+print(list)
+print(list[0])
+print(list[1])
+print(list[-1])
+print(list[-2])
+list[0]="HP"
+print(list[0])
+list[2][1]="python"
+print(list)
