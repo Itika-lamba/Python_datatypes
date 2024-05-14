1fb78031c834925e9368017f1e701c186362a8b6
Itika-Lamba <itika@trigvent.com>
2024-05-14 15:33:21 +0530

    created list tuple dict and set

---
+++ dict.py
@@ -0,0 +1,12 @@
+mydic ={
+    "name":"itika",
+    "age":23,
+    "role":"trainee",
+    "role":"software dev"
+}
+print(mydic)
+print(mydic["name"])
+mydic["age"]=22
+print(mydic)
+    
+
--- set.py
+++ set.py
@@ -1,4 +1,5 @@
-a={1,2,3,4,5}
+a={1,2,3,4,5,5}
+# still print 5 one time no duplicate allowed
 b={3,4,5,6,7}
 print(a)
 print(b)
@@ -6,3 +7,5 @@ print("Union of 2 set",a|b)
 print("Intersection of 2 set",a&b)
 print("Difference of 2 set",a-b)
 print("symmetric difference of 2 set",a^b)
+# a[2]=10 not allowed in case of set 
+print(type(a))
