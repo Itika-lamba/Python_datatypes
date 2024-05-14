889a138a47287d3ff0c023db95837b2ef96300d1
Itika-Lamba <itika@trigvent.com>
2024-05-14 15:17:34 +0530

    set data structure

---
+++ set.py
@@ -0,0 +1,8 @@
+a={1,2,3,4,5}
+b={3,4,5,6,7}
+print(a)
+print(b)
+print("Union of 2 set",a|b)
+print("Intersection of 2 set",a&b)
+print("Difference of 2 set",a-b)
+print("symmetric difference of 2 set",a^b)
